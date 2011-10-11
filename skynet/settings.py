403b1b20618e7d2d5c0e47a0c6ee103ba1b71ecc
Wytse Visser <wysevisser@gmail.com>
2011-10-11 13:46:05 +0200

    Project opgezet, inclusief South

--- skynet/settings.py
+++ skynet/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'south',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
