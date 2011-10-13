62b5379a51fb8645260a923f7b472c8bf90a2012
Tjeerd Jan <aidamina@gmail.com>
2011-10-13 12:03:11 +0200

    modified testrunner

--- frontend/skynet_frontend/settings.py
+++ frontend/skynet_frontend/settings.py
@@ -28,6 +28,8 @@ DATABASES = {
         'PORT': '',
     }
 }
+TEST_RUNNER = 'modified_testrunner.DjangoTestSuiteRunner'
+TEST_CREATE = False
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
