0248753f9fe41b358ef2861ec5771808716b7183
Daniel <daniel.j.a.van.den.brink@gmail.com>
2011-10-26 12:08:10 +0200

    backend iets

--- backend/skynet_backend/src/main/java/toctep/skynet/backend/dal/domain/BoundingBox.java
+++ backend/skynet_backend/src/main/java/toctep/skynet/backend/dal/domain/BoundingBox.java
@@ -1,13 +1,5 @@
 package toctep.skynet.backend.dal.domain;
 
 public class BoundingBox {
-
-	private BoundingBoxType boundingBoxType;
-	private String coordinates;
-	
-	public BoundingBox(BoundingBoxType boundingBoxType, String coordinates) {
-		this.boundingBoxType = boundingBoxType;
-		this.coordinates = coordinates;
-	}
 	
 }
