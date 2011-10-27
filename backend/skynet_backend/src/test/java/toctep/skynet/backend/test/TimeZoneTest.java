f62fb1fe63da8b69b869739df7657d56c390ac41
Maciek Dulkiewicz <maciek.t.d@gmail.com>
2011-10-27 15:47:23 +0200

    Domain tests, part 2

--- backend/skynet_backend/src/test/java/toctep/skynet/backend/test/TimeZoneTest.java
+++ backend/skynet_backend/src/test/java/toctep/skynet/backend/test/TimeZoneTest.java
@@ -1,5 +1,38 @@
 package toctep.skynet.backend.test;
 
-public class TimeZoneTest {
+import toctep.skynet.backend.dal.domain.TimeZone;
+
+public class TimeZoneTest extends DomainTest {
+
+	@Override
+	public void testCreate() {
+		TimeZone timeZone = new TimeZone();
+		assertNotNull(timeZone);
+		
+		String timeZoneString = "GMT";
+		int utcOffset = 2;
+		timeZone.setTimeZone(timeZoneString);
+		timeZone.setUtcOffset(utcOffset);
+		assertEquals("getTimeZone: ", timeZoneString, timeZone.getTimeZone());
+		assertEquals("getUtcOffset: ", utcOffset, timeZone.getUtcOffset());
+	}
+
+	@Override
+	public void testInsert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void testUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void testDelete() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- backend/skynet_backend/src/test/java/toctep/skynet/backend/test/URLTest.java
+++ backend/skynet_backend/src/test/java/toctep/skynet/backend/test/URLTest.java
@@ -1,5 +1,36 @@
 package toctep.skynet.backend.test;
 
-public class URLTest {
+import toctep.skynet.backend.dal.domain.URL;
+
+public class URLTest extends DomainTest {
+	
+	@Override
+	public void testCreate() {
+		URL url = new URL();
+		assertNotNull(url);
+		assertNull(url.getText());
+		
+		String urlText = "htt://www.diablo3.com";
+		url.setText(urlText);
+		assertEquals("getText result: ", urlText, url.getText());
+	}
+
+	@Override
+	public void testInsert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void testUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void testDelete() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
