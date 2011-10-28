7a8918bea40acda6b05c3e68df0265c8b564a4ab
Maciek Dulkiewicz <maciek.t.d@gmail.com>
2011-10-28 11:46:05 +0200

    TweetSplitter toegevoegd

--- backend/skynet_backend/src/test/java/toctep/skynet/backend/test/TweetSplitterTest.java
+++ backend/skynet_backend/src/test/java/toctep/skynet/backend/test/TweetSplitterTest.java
@@ -1,12 +1,45 @@
 package toctep.skynet.backend.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import toctep.skynet.backend.bll.TweetSplitter;
+
 public class TweetSplitterTest {
+	private TweetSplitter tweetSplitter;
+	
+	@Before
+	public void setUp() {
+		tweetSplitter = new TweetSplitter();
+	}
 	
 	@Test
 	public void splitTweetTest() {
+		String tweetBody = "@georgeBaker looking back on the track for a little green bag #muziek";
+		String[] presplitTweetBody = { 
+				"@georgeBaker", 
+				"looking", 
+				"back", 
+				"on", 
+				"the",
+				"track", 
+				"for",
+				"a",
+				"little",
+				"green",
+				"bag",
+				"#muziek"
+		};
 		
+		String[] tweetWordsAfterSplit = {};
+		
+		tweetWordsAfterSplit = tweetSplitter.splitTweet(tweetBody);
+		
+		for(int i = 0; i < presplitTweetBody.length; i++) {
+			assertEquals("result: ", presplitTweetBody[i], tweetWordsAfterSplit[i]);
+		}
 	}
 
 }
