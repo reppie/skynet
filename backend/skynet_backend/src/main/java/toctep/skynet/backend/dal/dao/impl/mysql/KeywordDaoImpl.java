9523b3bb403c1bc7fa8ea523ced28f6b262b0e61
Maciek Dulkiewicz <maciek.t.d@gmail.com>
2011-10-28 13:28:01 +0200

    Vergeten keyword en tweetKeyword te adden, tevens getters en setters gegeven

---
+++ backend/skynet_backend/src/main/java/toctep/skynet/backend/dal/dao/impl/mysql/KeywordDaoImpl.java
@@ -0,0 +1,32 @@
+package toctep.skynet.backend.dal.dao.impl.mysql;
+
+import toctep.skynet.backend.dal.dao.KeywordDao;
+import toctep.skynet.backend.dal.domain.Domain;
+
+public class KeywordDaoImpl extends KeywordDao {
+
+	@Override
+	public void insert(Domain domain) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Domain select(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Domain domain) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Domain domain) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
