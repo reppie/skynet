60db7f08647224af33410a343f5ea8f15cc2d871
Daniel <daniel.j.a.van.den.brink@gmail.com>
2011-11-01 12:24:17 +0100

    logger

--- backend/skynet_backend/src/main/java/toctep/skynet/backend/Main.java
+++ backend/skynet_backend/src/main/java/toctep/skynet/backend/Main.java
@@ -7,10 +7,13 @@ import toctep.skynet.backend.bll.TweetRetriever;
 
 public class Main {
 
+	public static final String DB_PROPERTIES = "mysql.properties";
+	public static final String LOG_PROPERTIES = "log4j.properties";
+	
 	static final Logger logger = Logger.getLogger(Main.class);
 	
 	public static void main(String[] args) {
-		PropertyConfigurator.configure("log4j.properties");
+		PropertyConfigurator.configure(LOG_PROPERTIES);
 				
 		new Thread(new TweetRetriever()).start();
 	}
