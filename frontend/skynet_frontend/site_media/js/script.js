a52d49ed35f6e4c9f1267ce90cdcbdb33e78220e
unknown <aidamina@gmail.com>
2011-11-03 16:42:34 +0100

    test

--- frontend/skynet_frontend/site_media/js/script.js
+++ frontend/skynet_frontend/site_media/js/script.js
@@ -5,7 +5,7 @@ $(function(){
 		
 		
 		var $searchbar = $("form#keyword-search-form").find("input#searchbar");
-		$searchbar.addClass("loading");
+		
 		var filters = [].concat(crumblePath.path());
 		var search = $searchbar.val();
 		var query = search.split(' ');
@@ -31,34 +31,7 @@ $(function(){
 		
 		
 		var $searchbar = $(this).find("input#searchbar");
-		/*
-		$(".main-tag-cloud").hide();
-		
-		var $searchbar = $(this).find("input#searchbar");
 		$searchbar.addClass("loading");
-		var filters = [].concat(crumblePath.path());
-		var search = $searchbar.val();
-		var query = search.split(' ');
-		for(var index in query){
-			var value = query[index];
-			if(value.length>=2){
-				var filter = null;
-				if(value.substring(0,1)=='@'){
-					filter = new api.filters.User(value.substring(1));
-				} if(value.substring(0,1)=='^'){
-					filter = new api.filters.Geo(value.substring(1), "NL", value.substring(1));
-				} else {
-					filter = new api.filters.Keyword(value);
-				}
-				filters.push(filter);
-			}
-		}
-		for (var index in filters){
-			var filter = filters[index];
-			if(filter.type=='geo'){
- 				$("section#region-name").find("p#current-region").html(filter.label);
- 			}
-		}*/
 		
 		var filters = getFilters();
 		for (var index in filters){
@@ -105,15 +78,15 @@ $(function(){
 		
 		var index = $(this).data("index");
 		
-		
 		var clicked = crumblePath.path()[index];
 		crumblePath.removeAfter(clicked);
-		
+		console.log(index);
 		if(index==0){
+			$search.find("input#searchbar").val("");
 			$(".tweets").hide().empty();
 			$("section#tag-cloud").hide();
 			$(".main-tag-cloud").show();
-		}else{
+		} else {
 			$search.submit();
 		}
 		
@@ -124,9 +97,6 @@ $(function(){
  				$("section#region-name").find("p#current-region").html(filter.label);
  			}
 		}
-		
-		
-		
 		return false;
 	});
 });
