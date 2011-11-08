c658cec7cdda6d7d5910b513bdf35fbab757d831
Steven <repsels@gmail.com>
2011-11-08 14:27:57 +0100

    Refactor

--- backend/skynet_backend/src/main/java/toctep/skynet/backend/bll/TweetUtils.java
+++ backend/skynet_backend/src/main/java/toctep/skynet/backend/bll/TweetUtils.java
@@ -6,13 +6,13 @@ import java.util.Date;
 
 public class TweetUtils {
 	
-	private TweetUtils() {}
-	
-	private static Calendar c = Calendar.getInstance();
 	public synchronized static Timestamp createUTCTimeStamp(Date date){
 		c.setTime(date);
 		Timestamp ts = new Timestamp((long)(c.getTime().getTime()-c.getTimeZone().getRawOffset()));
 		return ts;
 	}
 
+	private TweetUtils() {}
+	
+	private static Calendar c = Calendar.getInstance();
 }
