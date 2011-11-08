2b3d0e3902e592a9a1246306895143a72284046f
Daniel <daniel.j.a.van.den.brink@gmail.com>
2011-11-08 14:43:38 +0100

    sonar fixes

--- backend/skynet_backend/src/main/java/toctep/skynet/backend/bll/UserBioTweetRetriever.java
+++ backend/skynet_backend/src/main/java/toctep/skynet/backend/bll/UserBioTweetRetriever.java
@@ -11,6 +11,10 @@ public class UserBioTweetRetriever extends TweetRetriever {
 	
 	public static final String LANGDETECT_PROFILE_DIR = "lib/langdetect/profiles";
 	
+	public UserBioTweetRetriever() {
+		initialize();
+	}
+	
 	@Override
 	public void initialize() {
 		super.initialize();
