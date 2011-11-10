43294752cbc1bd96b873da1c9643ebf814c8f0ac
Maciek Dulkiewicz <maciek.t.d@gmail.com>
2011-11-10 12:28:09 +0100

    Toggle button voor time sliders toegevoegd

--- frontend/skynet_frontend/site_media/js/script.js
+++ frontend/skynet_frontend/site_media/js/script.js
@@ -182,5 +182,19 @@ $(function() {
 	updateTimeValues();
 });
 
+$( ".time-sliders" ).slider({
+		range: true,
+		min: 0,
+		max: 30,
+		values: [ 3, 28 ],
+		slide: function( event, ui ) {
+			$( ".time-value" ).html( "Toon tweets van: " + ui.values[ 0 ] + " tot: " + ui.values[ 1 ] );
+		}
+	});
+	$( ".time-value" ).html( "Toon tweets van: " +  $( ".time-sliders" ).slider( "values", 0 ) +
+		" tot: " + $( ".time-sliders" ).slider( "values", 1 ) );
+	$("#timebutton").click(function() {
+		$("#slider-container").toggle();
+	})
 
 
